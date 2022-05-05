d4c4494322b8f037231b2772698c7d69d6738a8e
angineeer <32205189+angineeer@users.noreply.github.com>
2022-05-06 01:14:41 +0300

    [GET] user/contracts endpoint fixed

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -124,10 +124,26 @@ router.delete("/pet", middlewares.verifyJWT, (req, res) => {
 //* ------------------------------------------------------------------------------------------
 
 router.get("/contract", middlewares.verifyJWT, (req, res) => {
-    Contract.find({ $or: [{ ownerId: req.user._id }, { hostId: req.user._id },] }).populate("pets").populate("ownerId").populate("hostId").then(contract => {
-        res.status(200).json(contract);
+    Host.findOne({ userId: req.user._id }).then(host => {
+        if(host){
+            Contract.find({ $or: [{ ownerId: req.user._id }, { hostId: host._id },] }).populate("pets").populate("ownerId").populate("hostId").then(contract => {
+                res.status(200).json(contract);
+            }).catch(err => {
+                res.status(500).json({ error: 'Contracts could not be found' });
+            });
+        } else {
+            Contract.find({ ownerId: req.user._id }).populate("pets").populate("ownerId").populate("hostId").then(contract => {
+                res.status(200).json(contract);
+            }).catch(err => {
+                res.status(500).json({ error: 'Contracts could not be found' });
+            });
+        }
     }).catch(err => {
-        res.status(500).json({ error: 'Contracts could not be found' });
+        Contract.find({ ownerId: req.user._id }).populate("pets").populate("ownerId").populate("hostId").then(contract => {
+            res.status(200).json(contract);
+        }).catch(err => {
+            res.status(500).json({ error: 'Contracts could not be found' });
+        });
     });
 });
 
